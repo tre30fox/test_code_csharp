8a11100078ccaca5f8809d7364615a95f0040a36
tre30fox <tre30fox@gmail.com>
2016-08-23 13:51:07 +0900

    Add files via upload
    init

---
+++ Reverse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    class Program
+    {
+        public static String reverse(char[] arrData)
+        {
+            int nLen = arrData.Length;
+            for (int i = 0; i < nLen / 2; ++i)
+            {
+                char temp = arrData[i];
+                arrData[i] = arrData[nLen - i - 1];
+                arrData[nLen - i - 1] = temp;
+            }
+
+            return new String(arrData);
+        }
+
+        static void Main(string[] args)
+        {
+            char[] test1 = "1234".ToCharArray();
+            Console.WriteLine(reverse(test1).Equals("4321") ? "PASS" : "FAIL");
+
+            char[] test2 = "aBc".ToCharArray();
+            Console.WriteLine(reverse(test2).Equals("cBa") ? "PASS" : "FAIL");
+
+            char[] test3 = "z3".ToCharArray();
+            Console.WriteLine(reverse(test3).Equals("3z") ? "PASS" : "FAIL");
+
+            char[] test4 = "q".ToCharArray();
+            Console.WriteLine(reverse(test4).Equals("q") ? "PASS" : "FAIL");
+
+            char[] test5 = "vtdqj2".ToCharArray();
+            Console.WriteLine(reverse(test5).Equals("2jqdtv") ? "PASS" : "FAIL");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rotate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Rotate
+{
+    class Rotate
+    {
+        static bool rotate(String strSrc, String strRotated)
+        {
+
+            char[] arrSrc = strSrc.ToCharArray();
+            char[] arrRotated = strRotated.ToCharArray();
+
+            if (arrSrc.Length != arrRotated.Length)
+            {
+                return false;
+            }
+
+            int nLen = arrSrc.Length;
+
+            for (int nShift = 0; nShift < nLen; ++nShift)
+            {
+                bool bResult = true;
+                for (int nCol = 0; nCol < nLen; ++nCol)
+                {
+                    if (arrSrc[nCol] != arrRotated[(nCol + nShift) % nLen])
+                    {
+                        bResult = false;
+                        break;
+                    }
+                }
+                if (bResult)
+                    return true;
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(rotate("abc", "cab") == true ? "PASS" : "FAIL");
+            Console.WriteLine(rotate("abc", "bca") == true ? "PASS" : "FAIL");
+            Console.WriteLine(rotate("abc", "abc") == true ? "PASS" : "FAIL");
+            Console.WriteLine(rotate("abc", "bac") == false ? "PASS" : "FAIL");
+            Console.WriteLine(rotate("abc", "acb") == false ? "PASS" : "FAIL");
+
+            Console.WriteLine(rotate("1324", "1324") == true ? "PASS" : "FAIL");
+            Console.WriteLine(rotate("1324", "4132") == true ? "PASS" : "FAIL");
+            Console.WriteLine(rotate("1324", "2413") == true ? "PASS" : "FAIL");
+            Console.WriteLine(rotate("1324", "3241") == true ? "PASS" : "FAIL");
+            Console.WriteLine(rotate("1324", "1234") == false ? "PASS" : "FAIL");
+            Console.WriteLine(rotate("1324", "2314") == false ? "PASS" : "FAIL");
+
+            Console.ReadLine();
+        }
+    }
+}
